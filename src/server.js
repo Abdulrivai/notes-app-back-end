65995a73065ce6ebb145484c15ca59806e8b71f4
Abdul Rivai <perabdulanduniawi20@gmail.com>
2025-07-22 21:50:42 +0700

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,26 @@
+const Hapi = require('@hapi/hapi');
+const notes = require('./api/notes');
+const NoteService = require('./service/inMemory/NodeService');
+
+const init = async () => {
+  const noteService = new NoteService();
+  const server = Hapi.server({
+    port: 3000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: { origin: ['*'] },
+    },
+  });
+
+  await server.register({
+    plugin: notes,
+    options: {
+      service: noteService,
+    },
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
